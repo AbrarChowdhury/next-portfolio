69aed574e602962104145963634f2e34993c318c
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-16 13:33:57 +0600

    update about

--- components/About.js
+++ components/About.js
@@ -1,20 +1,21 @@
-import Image from 'next/image'
-import React from 'react'
-import styles from './About.module.scss'
+import Image from "next/image"
+import React from "react"
+import styles from "./About.module.scss"
 const About = () => {
   return (
     <div className={styles.about}>
       <h1>About Me</h1>
-      <div style={{display:"flex"}}>
+      <div style={{ display: "flex" }}>
         {/* <Image src="/jack.jpg" alt="none" width={1000} height={900} /> */}
-        <div style={{minWidth:"50px", borderTop:"2px solid black", translate:"0 40px"}}>
-
-        </div>
-        <p style={{ fontSize: "1.5em", textAlign:"center" }}>
-          My name is <strong>Abrar Shams Chowdhury</strong>, currently working as a software engineer at Bioforge Health Systems LTD. I am an engineer by heart,
-          the thrill of inventing, designing, analyzing, and building keeps my heart ticking. I have experienced wearing <strong>many different hats</strong>{" "}
-          before finally deciding to <strong>master software engineering</strong>. Software Engineering allows me to solve everyday problems more efficiently and I love it!. I look forward to every
-          new opportunity with an open mind, with the goal of learning new things and improving each day.
+        <div
+          style={{
+            minWidth: "50px",
+            borderTop: "2px solid black",
+            translate: "0 40px",
+          }}
+        ></div>
+        <p style={{ fontSize: "1.5em", textAlign: "center" }}>
+        I see my work as an art form, with programming as my tool. My inspiration comes from all areas of my life, like using soccer strategies to tackle coding challenges. This creative mindset, combined with my diverse background, enables me to deliver solutions that are both <strong>innovative</strong> and <strong>effective</strong>. With a strong growth mentality and disciplined approach, I constantly seek to improve my skills and knowledge, ensuring that my work not only meets but <strong>exceeds expectations</strong>.
         </p>
       </div>
     </div>
