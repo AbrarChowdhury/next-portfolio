10a86696b12a6d432adfc8da91bc084408f3f60a
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-17 10:13:02 +0600

    show hide feature for experience section

--- components/ExperienceItem.js
+++ components/ExperienceItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import ProjectItem from "./ProjectItem"
 
 const ExperienceItem = ({
@@ -7,32 +8,81 @@ const ExperienceItem = ({
   responsibilities,
   projects,
 }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
   return (
-    <div style={{ borderLeft: "5px solid #abdcec", position: "relative" }}>
+    <div
+      style={{
+        position: "relative",
+        borderBottom: "5px solid #abdcec",
+        borderLeft: "5px solid #abdcec",
+      }}
+    >
       <img
         src='clock.svg'
         width='60px'
         style={{ position: "absolute", left: "-32.5px" }}
       />
       <div style={{ display: "flex" }}>
-        <div style={{ flex: 1, marginTop: "15px" }}>
-          <span style={{ marginLeft: "55px", fontSize: "2em" }}>{date}</span>
-          <img
-            style={{ display: "inline", width: "100px" }}
-            src='arrow.svg'
-          ></img>
+        <div
+          style={{
+            flex: 1,
+            marginTop: "15px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <span style={{ marginLeft: "55px", fontSize: "2em" }}>{date}</span>
+            <img
+              style={{ display: "inline", width: "100px" }}
+              src='arrow.svg'
+            ></img>
+          </div>
+          <div
+            onClick={toggleCollapse}
+            style={{
+              textAlign: "center",
+              borderTop: "5px solid #abdcec",
+              borderRight: "5px solid #abdcec",
+              borderBottom: "5px solid #abdcec",
+            }}
+          >
+            {isCollapsed ? "Show Projects" : "Hide Projects"}
+          </div>
         </div>
 
-        <div style={{ flex: 1 }}>
-          <p>{company}</p>
-          <p>{role}</p>
+        <div
+          style={{
+            flex: 1,
+            marginTop: "10px",
+            borderBottom: "5px solid #abdcec",
+          }}
+        >
+          <h3 style={{ marginBottom: "5px", textTransform: "uppercase" }}>
+            {company}
+          </h3>
+          <h2 style={{ marginTop: "0px" }}>{role}</h2>
           <p>{responsibilities}</p>
         </div>
       </div>
-      <div style={{ margin: "0px 55px" }}>
-        {projects.map((project, index) => (
-          <ProjectItem key={index} {...project} />
-        ))}
+      <div style={{ padding: "0px 55px" }}>
+        {!isCollapsed && (
+          <div>
+            {projects.map((project, index) => (
+              <ProjectItem
+                style={{ borderBottom: "5px solid #abdcec" }}
+                key={index}
+                {...project}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
