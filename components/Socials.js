dc495c4bc78afdfdb057f3f770e7ac99f8bc0dcc
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-27 12:40:11 +0600

    bring in new social icons

--- components/Socials.js
+++ components/Socials.js
@@ -7,14 +7,14 @@ const Socials = () => {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
-        width: "250px",
+        width: "200px",
       }}
     >
-      <Social src='socials/github.svg' href='www.hoglu.com' />
-      <Social src='socials/linkedin.svg' href='www.hoglu.com' />
-      <Social src='socials/leetcode.svg' href='www.hoglu.com' />
-      <Social src='socials/youtube.svg' href='www.hoglu.com' />
-      <Social src='socials/codepen.svg' href='www.hoglu.com' />
+      <Social src='socials/github.svg' href='https://github.com/AbrarChowdhury' />
+      <Social src='socials/linkedin.svg' href='https://www.linkedin.com/in/abrar-shams-chowdhury-48a979114/' />
+      <Social src='socials/whatsapp.svg' href='+8801836096182' />
+      <Social src='socials/email.svg' href='chowdhury.abrar.shams@gmail.com' />
+      {/* <Social src='socials/leetcode.svg' href='www.hoglu.com' /> */}
     </div>
   )
 }
