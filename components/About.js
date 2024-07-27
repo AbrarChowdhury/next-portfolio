30e6151a8bf52e7ec28d53f76a713d9a7317cf78
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-28 01:48:26 +0600

    change about

--- components/About.js
+++ components/About.js
@@ -6,16 +6,29 @@ const About = () => {
     <div className={styles.about}>
       <h1>About Me</h1>
       <div style={{ display: "flex" }}>
-        <p className="aboutFont">
-          I see my work as an art form, with programming as my tool. My
-          inspiration comes from all areas of my life, like using soccer
-          strategies to tackle coding challenges. This creative mindset,
-          combined with my diverse background, enables me to deliver solutions
-          that are both <strong>innovative</strong> and{" "}
-          <strong>effective</strong>. With a strong growth mentality and
-          disciplined approach, I constantly seek to improve my skills and
-          knowledge, ensuring that my work not only meets but{" "}
-          <strong>exceeds expectations</strong>.
+        <p className='aboutFont'>
+          I see my work as an art form, with programming as my number one tool.
+          Drawing inspiration from all sectors of my life, like using pro-artist
+          techniques to solve challenging programming problems. The creative
+          mindset, combined with my diverse background and disciplined approach, enables me to deliver
+           <strong>innovative</strong> and{" "}
+          <strong>effective</strong> solutions.<br /><br />
+          My style is to think like an <strong>artist</strong> but to to work like an <strong>accountant</strong>.
+          I don't believe in motivation or "feeling inspired", I only focus on building systems in order to get big things done.
+          <br /><br />
+          Despite the virtues, sometimes failure is just inevitable and I am
+          lucky to have had many pretty early in life. Every time such an events occured, I asked
+          myself "what did I learn in the process" and made sure to put that in
+          my journal.
+          <br /><br />
+          These failures benefited me in two ways. One I am not afraid of making
+          mistakes, which allows me to experiment and innovate. Two, I have
+          developed an intuition for when things are not going to work or what
+          needs to be done in order to make things work.
+          <br /><br />
+          Lastly, if theres one thing that makes me rare is my persistence. Where most
+          people give up my I will keep searching for solutions. I don't like uttering  "I can't".
+          Much like bob the builder. if you ask me "abrar can you fix/build it" my answer would be "yes I can".
         </p>
       </div>
     </div>
