eeb9541ef2800327599b101292fbe36fb9690faa
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-28 22:07:41 +0600

    refactor add tech icons and correct text

--- components/techIconList.js
+++ components/techIconList.js
@@ -1,102 +1,222 @@
 const virtyouTechList = [
-    {
-      href: "https://threejs.org",
-      src: "tech_icons/threejs.svg",
-      alt: "threejs",
-      name: "Three.js",
-    },
-    {
-      href: "https://immersiveweb.dev",
-      src: "tech_icons/webxr.svg",
-      alt: "webxr",
-      name: "WebXR",
-    },
-    {
-      href: "https://aws.amazon.com/polly/",
-      src: "tech_icons/awsPolly.svg",
-      alt: "awsPolly",
-      name: "AWS Polly",
-    },
-    {
-      href: "https://huggingface.co",
-      src: "tech_icons/huggingFace.svg",
-      alt: "huggingFace",
-      name: "Hugging Face",
-    },
-    {
-      href: "https://www.blender.org/features/",
-      src: "tech_icons/blender.svg",
-      alt: "blender",
-      name: "Blender",
-    },
-    {
-      href: "https://socket.io/docs/v4/",
-      src: "tech_icons/socketio.svg",
-      alt: "socketio",
-      name: "Socket.IO",
-    },
-    {
-      href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
-      src: "tech_icons/nodejs.svg",
-      alt: "nodejs",
-      name: "Node.js",
-    },
-  ]
-  
-  const reflectTechList = [
-    {
-      href: "https://reflect.tech",
-      src: "tech_icons/webex.svg",
-      alt: "reflectTech",
-      name: "Webex",
-    },
-    {
-      href: "https://www.blender.org/features/",
-      src: "tech_icons/meet.svg",
-      alt: "blender",
-      name: "Meet",
-    },
-    {
-      href: "https://socket.io/docs/v4/",
-      src: "tech_icons/gmail.svg",
-      alt: "socketio",
-      name: "Gmail",
-    },
-    {
-      href: "https://socket.io/docs/v4/",
-      src: "tech_icons/outlook.svg",
-      alt: "socketio",
-      name: "Outlook",
-    },
-    {
-      href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
-      src: "tech_icons/nodejs.svg",
-      alt: "nodejs",
-      name: "Node.js",
-    },
-  ]
-  
-  const hmsTechList = [
-    {
-      href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
-      src: "tech_icons/nodejs.svg",
-      alt: "nodejs",
-      name: "Node.js",
-    },
-  ]
-  
-  const vsmTechLists = [
-    {
-      href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
-      src: "tech_icons/nodejs.svg",
-      alt: "nodejs",
-      name: "Node.js",
-    },
-  ]
+  {
+    href: "https://threejs.org",
+    src: "tech_icons/threejs.svg",
+    alt: "threejs",
+    name: "Three.js",
+  },
+  {
+    href: "https://immersiveweb.dev",
+    src: "tech_icons/webxr.svg",
+    alt: "webxr",
+    name: "WebXR",
+  },
+  {
+    href: "https://aws.amazon.com/polly/",
+    src: "tech_icons/awsPolly.svg",
+    alt: "awsPolly",
+    name: "AWS Polly",
+  },
+  {
+    href: "https://huggingface.co",
+    src: "tech_icons/huggingFace.svg",
+    alt: "huggingFace",
+    name: "Hugging Face",
+  },
+  {
+    href: "https://www.blender.org/features/",
+    src: "tech_icons/blender.svg",
+    alt: "blender",
+    name: "Blender",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/socketio.svg",
+    alt: "socketio",
+    name: "Socket.IO",
+  },
+  {
+    href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
+    src: "tech_icons/nodejs.svg",
+    alt: "nodejs",
+    name: "Node.js",
+  },
+  {
+    href: "https://nextjs.org/",
+    src: "tech_icons/nextjs.svg",
+    alt: "next.js",
+    name: "Next.js",
+  },
+]
 
-  const incubatorTechList = []
+const langaraTechList = [
+  {
+    href: "https://immersiveweb.dev",
+    src: "tech_icons/webxr.svg",
+    alt: "webxr",
+    name: "WebXR",
+  },
+  {
+    href: "https://threejs.org",
+    src: "tech_icons/threejs.svg",
+    alt: "threejs",
+    name: "Three.js",
+  },
+  {
+    href: "https://www.blender.org/features/",
+    src: "tech_icons/blender.svg",
+    alt: "blender",
+    name: "Blender",
+  },
+  {
+    href: "https://aws.amazon.com/polly/",
+    src: "tech_icons/awsPolly.svg",
+    alt: "awsPolly",
+    name: "AWS Polly",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/socketio.svg",
+    alt: "socketio",
+    name: "Socket.IO",
+  },
+  {
+    href: "https://nextjs.org/",
+    src: "tech_icons/nextjs.svg",
+    alt: "next.js",
+    name: "Next.js",
+  },
+]
 
-  const ptlTechList = []
-  
-  export { virtyouTechList, reflectTechList, hmsTechList, vsmTechLists }
-  
+const mindbloomTechList = [
+  {
+    href: "https://threejs.org",
+    src: "tech_icons/threejs.svg",
+    alt: "threejs",
+    name: "Three.js",
+  },
+  {
+    href: "https://aws.amazon.com/polly/",
+    src: "tech_icons/awsPolly.svg",
+    alt: "awsPolly",
+    name: "AWS Polly",
+  },
+  {
+    href: "https://huggingface.co",
+    src: "tech_icons/huggingFace.svg",
+    alt: "huggingFace",
+    name: "Hugging Face",
+  },
+  {
+    href: "https://www.blender.org/features/",
+    src: "tech_icons/blender.svg",
+    alt: "blender",
+    name: "Blender",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/socketio.svg",
+    alt: "socketio",
+    name: "Socket.IO",
+  },
+  {
+    href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
+    src: "tech_icons/nodejs.svg",
+    alt: "nodejs",
+    name: "Node.js",
+  },
+]
+
+const reflectTechList = [
+  {
+    href: "https://reflect.tech",
+    src: "tech_icons/webex.svg",
+    alt: "reflectTech",
+    name: "Webex",
+  },
+  {
+    href: "https://www.blender.org/features/",
+    src: "tech_icons/meet.svg",
+    alt: "blender",
+    name: "Meet",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/gmail.svg",
+    alt: "socketio",
+    name: "Gmail",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/outlook.svg",
+    alt: "socketio",
+    name: "Outlook",
+  },
+  {
+    href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
+    src: "tech_icons/nodejs.svg",
+    alt: "nodejs",
+    name: "Node.js",
+  },
+]
+
+const hmsTechList = [
+  {
+    href: "https://react.dev/",
+    src: "tech_icons/react.svg",
+    alt: "react.js",
+    name: "React.js",
+  },
+  {
+    href: "https://mui.com/material-ui/",
+    src: "tech_icons/mui.svg",
+    alt: "Material UI",
+    name: "Material UI",
+  },
+  {
+    href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
+    src: "tech_icons/nodejs.svg",
+    alt: "nodejs",
+    name: "Node.js",
+  },
+  {
+    href: "https://www.postgresql.org/",
+    src: "tech_icons/postgresql.svg",
+    alt: "nodejs",
+    name: "PostgreSQL",
+  },
+  {
+    href: "https://www.mongodb.com/",
+    src: "tech_icons/mongo_leaf.svg",
+    alt: "mongodb",
+    name: "MongoDB",
+  },
+  {
+    href: "https://socket.io/docs/v4/",
+    src: "tech_icons/socketio.svg",
+    alt: "socketio",
+    name: "Socket.IO",
+  },
+]
+
+const vsmTechLists = [
+  {
+    href: "https://nodejs.org/en/learn/getting-started/introduction-to-nodejs",
+    src: "tech_icons/nodejs.svg",
+    alt: "nodejs",
+    name: "Node.js",
+  },
+]
+
+const incubatorTechList = []
+
+const ptlTechList = []
+
+export {
+  virtyouTechList,
+  langaraTechList,
+  mindbloomTechList,
+  reflectTechList,
+  hmsTechList,
+  vsmTechLists,
+}
