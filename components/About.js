874498abde8ae5839fa0a63aa93eb170135c60a4
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-07-31 15:01:30 +0600

    add more breaks to about

--- components/About.js
+++ components/About.js
@@ -38,10 +38,12 @@ const About = () => {
           enables me to deliver{" "}
           <strong>innovative and effective solutions</strong>.
           <br />
+          <br />
           My style is to{" "}
           <strong>think like an artist but work like an accountant</strong>. I
           don't believe in motivation or "feeling inspired;" I focus on{" "}
           <strong>building systems to get big things done</strong>.
+          <br />
           <br />
           Despite the virtues, sometimes failure is just inevitable, and I am
           lucky to have had many pretty early in life. Every time such an event
@@ -49,10 +51,12 @@ const About = () => {
           <strong>What did I learn in the process?</strong>" and made sure to
           put that in my journal.
           <br />
+          <br />
           These failures benefited me in two ways. First, I am not afraid of
           making mistakes, which allows me to experiment and innovate. Second, I
           have developed an intuition for when things are not going to work or
           what needs to be done to make things work.
+          <br />
           <br />
           Lastly, if there's one thing that makes me rare, it is my{" "}
           <strong>persistence</strong>. Where most people give up, I will keep
