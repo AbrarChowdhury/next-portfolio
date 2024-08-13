fc338dc8a204b94dde5bff8250a47deaa39c8386
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-08-13 13:49:41 +0600

    change button

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,11 +1,13 @@
 import React from "react"
 import styles from "./Button.module.css"
 
-const Button = ({ text,handleClick }) => {
+const Button = ({ text, handleClick }) => {
   return (
-    <button class={styles.button} role='button' onClick={handleClick}>
-      {text}
-    </button>
+
+      <button class={styles.button} role='button' onClick={handleClick}>
+        {text}
+      </button>
+
   )
 }
 
